47168efc9b1f58b3b0d0e80554454b3aba5c18d2
Tim Parkyn <timparkyn@gmail.com>
2018-04-22 21:17:31 -0700

    Fixed unselected station listing

--- app/controllers/selections_controller.rb
+++ app/controllers/selections_controller.rb
@@ -3,7 +3,9 @@ include SelectionsHelper
 class SelectionsController < ApplicationController
 
   def index
-    @selections = current_user.selections
+    @selections = current_user.stations
+    puts '* * ** * * ** * * ** * **'
+    puts @selections
     get_forecast(@selections)
 
     # why doesnt this work?
@@ -22,19 +24,12 @@ class SelectionsController < ApplicationController
   end
 
   def create
-    puts '* * * * * * * * * *'
-    puts params
     station = Station.find(params[:id])
     Selection.find_or_create_by(user_id: current_user.id, station_id: station.id)
     redirect_to user_selections_path
   end
 
   def destroy
-    puts '------------------------------'
-    # station = Station.find(selection_params[:id])
-    puts params
-    # puts Selection.find(user_id: current_user.id, station_id: station.id)
-    # Selection.find_by(user_id: current_user.id, station_id: station.id).destroy
     Selection.find(params[:id]).destroy
     redirect_to user_selections_path
   end
