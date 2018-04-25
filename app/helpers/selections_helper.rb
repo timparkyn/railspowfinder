5a65166af942023bc3e4ee038cbfe49dc48acdf6
Tim Parkyn <timparkyn@gmail.com>
2018-04-25 14:09:56 -0700

    Cleaned up old comments and outdated code

--- app/helpers/selections_helper.rb
+++ app/helpers/selections_helper.rb
@@ -29,29 +29,6 @@ module SelectionsHelper
           max_wind_mph: fx.maxwind.mph
         }
       end
-      # forecast = {
-      #   station_id: selection.id,
-      #   description: selection.description,
-      #   observations: response.current_observation,
-      #   elevation: response.current_observation.observation_location.elevation,
-      #   daily_fx: {
-      #     day: [],
-      #     pop: [],
-      #     qpf: [],
-      #     snow_in: [],
-      #     max_wind_mph: []
-      #   }
-      # }
-      #
-      # #response returns an array of forecast objects
-      # daily_forecasts = response.forecast.simpleforecast.forecastday
-      # daily_forecasts.each do |fx|
-      #   forecast[:daily_fx][:day].push(fx.date.weekday_short)
-      #   forecast[:daily_fx][:pop].push(fx.pop)
-      #   forecast[:daily_fx][:qpf].push(fx.qpf_allday.in) #.in
-      #   forecast[:daily_fx][:snow_in].push(fx.snow_allday.in)
-      #   forecast[:daily_fx][:max_wind_mph].push(fx.maxwind.mph) #.mpg
-      # end
       @forecasts.push(forecast)
     end
   end
