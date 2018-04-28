f82bd81cb93ef0e86d76de0ccf96107fd2137a87
Tim Parkyn <timparkyn@gmail.com>
2018-04-27 17:47:36 -0700

    Fix route for /signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,7 +15,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to @user
+      redirect_to user_selections_path
     else
       render 'new'
     end
--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,12 @@
 Rails.application.routes.draw do
 
-
-
-
-  get 'session/new'
-
   root 'static_pages#home'
-
+  get 'session/new'
   get 'users/new'
-
   get 'static_pages/home'
-
   get '/help', to: 'static_pages#help'
-
   get  '/signup',  to: 'users#new'
-
+  post '/signup', to: 'users#create'
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
